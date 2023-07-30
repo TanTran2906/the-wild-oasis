95ead652b7cfb2f53fff82655d12e034e58e5721
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2023-07-30 22:42:58 +0700

    mutate data - deleting

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -11,9 +14,18 @@ import PageNotFound from "./pages/PageNotFound";
 
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 0,
+        },
+    },
+});
+
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
@@ -34,7 +46,7 @@ function App() {
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
     );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ijzvdlcgvxyfrbcjjbvt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqenZkbGNndnh5ZnJiY2pqYnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA2MzE0MjIsImV4cCI6MjAwNjIwNzQyMn0.vn-eD7nvJtJ5lrGmr9RffdgRlFEnkaoFEaULJFmhSb4'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
