386a551da42add74ba82afc8bb869a76b9317f0f
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2023-08-01 23:32:53 +0700

    Edit/Create form

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -5,85 +5,211 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createEditCabin } from "../../services/apiCabins";
+import { toast } from "react-hot-toast";
 
 const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
+    display: grid;
+    align-items: center;
+    grid-template-columns: 24rem 1fr 1.2fr;
+    gap: 2.4rem;
 
-  padding: 1.2rem 0;
+    padding: 1.2rem 0;
 
-  &:first-child {
-    padding-top: 0;
-  }
+    &:first-child {
+        padding-top: 0;
+    }
 
-  &:last-child {
-    padding-bottom: 0;
-  }
+    &:last-child {
+        padding-bottom: 0;
+    }
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
+    &:not(:last-child) {
+        border-bottom: 1px solid var(--color-grey-100);
+    }
 
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
+    &:has(button) {
+        display: flex;
+        justify-content: flex-end;
+        gap: 1.2rem;
+    }
 `;
 
 const Label = styled.label`
-  font-weight: 500;
+    font-weight: 500;
 `;
 
 const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
+    font-size: 1.4rem;
+    color: var(--color-red-700);
 `;
 
-function CreateCabinForm() {
-  return (
-    <Form>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
-      </FormRow>
+function CreateCabinForm({ cabinToEdit = {} }) {
+    const { id: editId, ...editValues } = cabinToEdit;
+    const isEditSession = Boolean(editId);
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
-      </FormRow>
+    const { register, handleSubmit, reset, getValues, formState } = useForm({
+        defaultValues: isEditSession ? editValues : {},
+    });
+    const { errors } = formState;
+    const queryClient = useQueryClient();
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
-      </FormRow>
+    //mutate: createCabin
+    const { mutate: createCabin, isLoading: isCreating } = useMutation({
+        mutationFn: createEditCabin,
+        onSuccess: () => {
+            toast.success("New cabin successfully created");
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
-      </FormRow>
+            queryClient.invalidateQueries({ queryKey: ["cabins"] });
+            reset();
+        },
+        onError: (err) => toast.error(err.message),
+    });
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
-      </FormRow>
+    //mutate : editCabin
+    const { mutate: editCabin, isLoading: isEditing } = useMutation({
+        mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
+        onSuccess: () => {
+            toast.success("Cabin successfully edited");
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRow>
+            queryClient.invalidateQueries({ queryKey: ["cabins"] });
+            reset();
+        },
+        onError: (err) => toast.error(err.message),
+    });
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Edit cabin</Button>
-      </FormRow>
-    </Form>
-  );
+    const isWorking = isCreating || isEditing;
+
+    function onSubmit(data) {
+        const image =
+            typeof data.image === "string" ? data.image : data.image[0];
+
+        if (isEditSession)
+            editCabin({
+                newCabinData: { ...data, image },
+                id: editId,
+            });
+        else createCabin({ ...data, image });
+    }
+
+    function onError(errors) {
+        console.log(errors);
+    }
+
+    return (
+        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+            <FormRow>
+                <Label htmlFor="name">Cabin name</Label>
+                <Input
+                    type="text"
+                    id="name"
+                    disabled={isWorking}
+                    {...register("name", {
+                        required: "This field is required",
+                    })}
+                />
+                {errors?.name?.message && <Error>{errors.name.message}</Error>}
+            </FormRow>
+
+            <FormRow>
+                <Label htmlFor="maxCapacity">Maximum capacity</Label>
+                <Input
+                    type="number"
+                    id="maxCapacity"
+                    disabled={isWorking}
+                    {...register("maxCapacity", {
+                        required: "This field is required",
+                        min: {
+                            value: 1,
+                            message: "Capacity should be at least 1",
+                        },
+                    })}
+                />
+                {errors?.maxCapacity?.message && (
+                    <Error>{errors.maxCapacity.message}</Error>
+                )}
+            </FormRow>
+
+            <FormRow>
+                <Label htmlFor="regularPrice">Regular price</Label>
+                <Input
+                    type="number"
+                    id="regularPrice"
+                    disabled={isWorking}
+                    {...register("regularPrice", {
+                        required: "This field is required",
+                        min: {
+                            value: 1,
+                            message: "Regular price should be at least 1",
+                        },
+                    })}
+                />
+                {errors?.regularPrice?.message && (
+                    <Error>{errors.regularPrice.message}</Error>
+                )}
+            </FormRow>
+
+            <FormRow>
+                <Label htmlFor="discount">Discount</Label>
+                <Input
+                    type="number"
+                    id="discount"
+                    disabled={isWorking}
+                    defaultValue={0}
+                    {...register("discount", {
+                        required: "This field is required",
+                        validate: (value) =>
+                            Number(value) <= getValues().regularPrice ||
+                            "Discount should be less than regular price",
+                    })}
+                />
+                {errors?.discount?.message && (
+                    <Error>{errors.discount.message}</Error>
+                )}
+            </FormRow>
+
+            <FormRow>
+                <Label htmlFor="description">Description for website</Label>
+                <Textarea
+                    type="number"
+                    id="description"
+                    disabled={isWorking}
+                    defaultValue=""
+                    {...register("description", {
+                        required: "This field is required",
+                    })}
+                />
+                {errors?.description?.message && (
+                    <Error>{errors.description.message}</Error>
+                )}
+            </FormRow>
+
+            <FormRow>
+                <Label htmlFor="image">Cabin photo</Label>
+                <FileInput
+                    id="image"
+                    accept="image/*"
+                    {...register("image", {
+                        required: isEditSession
+                            ? false
+                            : "This field is required",
+                    })}
+                />
+            </FormRow>
+
+            <FormRow>
+                {/* type is an HTML attribute! */}
+                <Button variation="secondary" type="reset">
+                    Cancel
+                </Button>
+                <Button disabled={isWorking}>
+                    {isEditSession ? "Edit cabin" : "Create new cabin"}
+                </Button>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default CreateCabinForm;
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,10 +1,11 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
 
     const { data, error } = await supabase
         .from('cabins')
         .select('*')
+    // .order("created_at", { ascending: true }); //Cái mới nhất sẽ ở trên đầu
 
     if (error) {
         console.error(error)
@@ -23,6 +24,60 @@ export async function deleteCabin(id) {
     if (error) {
         console.error(error)
         throw new Error('Cabins could not be deleted')
+    }
+
+    return data
+}
+
+export async function createEditCabin(newCabin, id) {
+    // console.log(newCabin, id)
+    const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
+
+    //1)Create/edit Cabin
+    //Xử lý đầu vào tên của ảnh khi người dùng upload --> Đảm bảo ảnh là duy nhất trong storage
+    const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+        "/",
+        ""
+    );
+
+    //Ảnh tồn tại dạng Url ở supabase: https://ijzvdlcgvxyfrbcjjbvt.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg --> Tuân theo định dạng này
+    //Xử lý 2 TH:
+    //TH1: image url không ở dạng FileList vì lấy url mặc định có trong CSDL(tức lấy image cũ submit luôn mà không upload ảnh mới)
+    //TH2: upload image mới
+    const imagePath = hasImagePath
+        ? newCabin.image
+        : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+    let query = supabase.from('cabins')
+
+    //A)Create cabin
+    if (!id) query = query.insert([
+        { ...newCabin, image: imagePath }])
+
+    //B)Edit cabin
+    if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id)
+
+    const { data, error } = await query.select().single()
+    //select().single() được sử dụng sau khi thêm dữ liệu vào bảng cabins để lấy lại dữ liệu của bản ghi mới nhất vừa được thêm thành công vào bảng vì Supabase không trả về dữ liệu của bản ghi sau khi thêm vào bảng(chậm 1 nhịp).
+
+    if (error) {
+        console.error(error)
+        throw new Error('Cabins could not be inserted')
+    }
+
+    //2)Upload image
+    if (hasImagePath) return data;
+    const { error: storageError } = await supabase.storage.from('cabin-images').upload(imageName, newCabin.image)
+
+    //3)Delete the cabin IF there was an error uploading image
+    if (storageError) {
+        await supabase
+            .from('cabins')
+            .delete()
+            .eq('id', data.id)
+        console.error(storageError)
+        throw new Error("Cabin image could not be uploaded and the cabin was not created")
     }
 
     return data
