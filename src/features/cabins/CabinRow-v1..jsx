40709381df656ba414ede4c742cf6d5c2c860d6b
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2023-08-05 01:19:18 +0700

    Menu-Compound

---
+++ src/features/cabins/CabinRow-v1..jsx
@@ -0,0 +1,146 @@
+import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createEditCabin, deleteCabin } from "../../services/apiCabins";
+import { toast } from "react-hot-toast";
+import CreateCabinForm from "./CreateCabinForm";
+import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
+
+// const TableRow = styled.div`
+//     display: grid;
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//     column-gap: 2.4rem;
+//     align-items: center;
+//     padding: 1.4rem 2.4rem;
+
+//     &:not(:last-child) {
+//         border-bottom: 1px solid var(--color-grey-100);
+//     }
+// `;
+
+const Img = styled.img`
+    display: block;
+    width: 6.4rem;
+    aspect-ratio: 3 / 2;
+    object-fit: cover;
+    object-position: center;
+    transform: scale(1.5) translateX(-7px);
+`;
+
+const Cabin = styled.div`
+    font-size: 1.6rem;
+    font-weight: 600;
+    color: var(--color-grey-600);
+    font-family: "Sono";
+`;
+
+const Price = styled.div`
+    font-family: "Sono";
+    font-weight: 600;
+`;
+
+const Discount = styled.div`
+    font-family: "Sono";
+    font-weight: 500;
+    color: var(--color-green-700);
+`;
+
+function CabinRow({ cabin }) {
+    const {
+        id: cabinId,
+        name,
+        maxCapacity,
+        regularPrice,
+        discount,
+        image,
+        description,
+    } = cabin;
+
+    const queryClient = useQueryClient();
+
+    const { isLoading: isDeleting, mutate } = useMutation({
+        mutationFn: deleteCabin,
+        onSuccess: () => {
+            toast.success("Cabin successfully deleted");
+
+            queryClient.invalidateQueries({
+                queryKey: ["cabins"],
+            });
+        },
+        onError: (err) => toast.error(err.message),
+    });
+
+    const { mutate: createCabin, isLoading: isCreating } = useMutation({
+        mutationFn: createEditCabin,
+        onSuccess: () => {
+            toast.success("New cabin successfully created");
+
+            queryClient.invalidateQueries({ queryKey: ["cabins"] });
+        },
+        onError: (err) => toast.error(err.message),
+    });
+
+    return (
+        <Table.Row>
+            <Img src={image} />
+            <Cabin>{name}</Cabin>
+            <div>Fits up to {maxCapacity}</div>
+            <Price>{formatCurrency(regularPrice)}</Price>
+            {discount ? (
+                <Discount>{formatCurrency(discount)}</Discount>
+            ) : (
+                <span>&mdash;</span>
+            )}
+            <div>
+                <button
+                    disabled={isCreating}
+                    //Tạo 1 bản sao của cabin , không chuyển id vào {} vì id là duy nhất
+                    onClick={() =>
+                        createCabin({
+                            name: `Copy of ${name}`,
+                            maxCapacity,
+                            regularPrice,
+                            discount,
+                            image,
+                            description,
+                        })
+                    }
+                >
+                    <HiSquare2Stack />
+                </button>
+
+                <Modal>
+                    <Modal.Open opens="edit">
+                        <button>
+                            <HiPencil />
+                        </button>
+                    </Modal.Open>
+                    <Modal.Window name="edit">
+                        <CreateCabinForm cabinToEdit={cabin} />
+                    </Modal.Window>
+                </Modal>
+
+                <Modal>
+                    <Modal.Open opens="confirm-delete">
+                        <button>
+                            <HiTrash />
+                        </button>
+                    </Modal.Open>
+                    <Modal.Window name="confirm-delete">
+                        <ConfirmDelete
+                            resourceName="cabins"
+                            disabled={isDeleting}
+                            onConfirm={() => mutate(cabinId)}
+                        />
+                    </Modal.Window>
+                </Modal>
+            </div>
+        </Table.Row>
+    );
+}
+
+export default CabinRow;
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,11 @@
+import {
+    cloneElement,
+    createContext,
+    useContext,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -51,20 +59,71 @@ const Button = styled.button`
     }
 `;
 
-function Modal({ children, onClose }) {
+const ModalContext = createContext();
+
+function Modal({ children }) {
+    const [openName, setOpenName] = useState("");
+
+    const close = () => setOpenName("");
+    const open = setOpenName;
+
+    return (
+        <ModalContext.Provider
+            value={{
+                openName,
+                close,
+                open,
+            }}
+        >
+            {children}
+        </ModalContext.Provider>
+    );
+}
+
+function Open({ children, opens: opensWindowName }) {
+    const { open } = useContext(ModalContext);
+
+    return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+    const { openName, close } = useContext(ModalContext);
+
+    const ref = useRef();
+    useEffect(
+        function () {
+            function handleClick(e) {
+                if (ref.current && !ref.current.contains(e.target)) {
+                    close();
+                }
+            }
+
+            document.addEventListener("click", handleClick, true);
+
+            return () =>
+                document.removeEventListener("click", handleClick, true);
+        },
+        [close]
+    );
+
+    if (openName !== name) return null;
+
     return createPortal(
         <Overlay>
-            <StyledModal>
-                <Button onClick={onClose}>
+            <StyledModal ref={ref}>
+                <Button onClick={close}>
                     <HiXMark />
                 </Button>
 
-                <div>{children}</div>
+                <div>{cloneElement(children, { onCloseModal: close })}</div>
             </StyledModal>
         </Overlay>,
         document.body
         // document.querySelector()
     );
 }
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
