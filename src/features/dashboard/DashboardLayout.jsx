1206bc8b22a76a5440f09060f53cb91e71816034
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2023-08-13 22:05:45 +0700

    Statistic

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -8,9 +13,26 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+    const { bookings, isLoading: isLoading1 } = useRecentBookings();
+    const {
+        stays,
+        confirmedStays,
+        isLoading: isLoading2,
+        numDays,
+    } = useRecentStays();
+    const { isLoading: isLoading3, cabins } = useCabins();
+
+    if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+    // console.log(stays, confirmedStays);
     return (
         <StyledDashboardLayout>
-            <div>Statistic</div>
+            <Stats
+                bookings={bookings}
+                confirmedStays={confirmedStays}
+                numDays={numDays}
+                cabinCount={cabins.length}
+            />
             <div>Today's activity</div>
             <div>Chart stay duration</div>
             <div>Chart sales</div>
